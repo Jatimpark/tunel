07fbd33eefeb0289f1435765721563ff76cd2bc3
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2023-09-04 20:03:21 +0700

    Add files via upload

--- backup/restore.sh
+++ backup/restore.sh
@@ -2,7 +2,7 @@
 # ==========================================
 # Color
 RED='\033[0;31m'
-NC='\033[0;37m'
+NC='\033[0m'
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
@@ -12,54 +12,32 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 clear
-echo "Fitur Ini Hanya Bisa Digunakan Sesuai Data Vps Dengan Autoscript Ini"
-echo "Silakan masukkan tautan ke file cadangan data vps Anda "
-echo "Anda dapat memeriksanya di email Anda jika Anda menjalankan backup data vps sebelumnya "
-echo " "
+echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
+echo "Please input link to your vps data backup file."
+echo "You can check it on your email if you run backup data vps before."
 read -rp "Link File: " -e url
 wget -O backup.zip "$url"
 unzip backup.zip
 rm -f backup.zip
 sleep 1
 echo Start Restore
-rm -f /root/backup/backup.zip &> /dev/null
-sleep 1
 cd /root/backup
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring passwd data..."
-sleep 1
-cp /root/backup/passwd /etc/ &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring group data..."
-sleep 1
-cp /root/backup/group /etc/ &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring shadow data..."
-sleep 1
-cp /root/backup/shadow /etc/ &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring gshadow data..."
-sleep 1
-cp /root/backup/gshadow /etc/ &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring chap-secrets data..."
-sleep 1
-cp /root/backup/chap-secrets /etc/ppp/ &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring passwd1 data..."
-sleep 1
-cp /root/backup/passwd1 /etc/ipsec.d/passwd &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring ss.conf data..."
-sleep 1
-cp /root/backup/ss.conf /etc/shadowsocks-libev/ss.conf &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring admin data..."
-sleep 1
-cp -r /root/backup/scrz-prem /var/lib/ &> /dev/null
-cp -r /root/backup/.acme.sh /root/ &> /dev/null
-cp -r /root/backup/AryaVpn /etc/ &> /dev/null
-cp -r /root/backup/xray /etc/ &> /dev/null
-cp -r /root/backup/public_html /home/vps/ &> /dev/null
-cp -r /root/backup/crontab /etc/ &> /dev/null
-cp -r /root/backup/cron.d /etc/ &> /dev/null
-rm -rf /root/backup &> /dev/null
+cp passwd /etc/
+cp group /etc/
+cp shadow /etc/
+cp gshadow /etc/
+cp -r wireguard /etc/
+cp chap-secrets /etc/ppp/
+cp passwd1 /etc/ipsec.d/passwd
+cp ss.conf /etc/shadowsocks-libev/ss.conf
+cp -r scrz-prem /var/lib/
+cp -r sstp /home/
+cp -r xray /etc/
+cp -r trojan-go /etc/
+cp -r shadowsocksr /usr/local/
+cp -r public_html /home/vps/
+cp crontab /etc/
+strt
 rm -rf /root/backup
-echo -e "$COLOR1│${NC}  [ ${GREEN}INFO${NC} ] • Done... Successfully"
 rm -f backup.zip
 echo Done
-echo ""
-read -n 1 -s -r -p "   Press any key to back on menu"
-menu
