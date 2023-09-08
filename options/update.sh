11084bc74257083b6bda48ea8ed01416c1315184
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2023-09-08 14:55:09 +0700

    Add files via upload

--- options/update.sh
+++ options/update.sh
@@ -1,20 +1,101 @@
 #!/bin/bash
-CYAN='\e[0;36m'
-GREEN='\e[0;32m'
-NC='\e[0;37m'
+#wget https://github.com/${GitUser}/
+GitUser="Jatimpark"
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+echo ""
+version=$(cat /home/ver)
+ver=$( curl https://raw.githubusercontent.com/${GitUser}/tunel/main/version )
 clear
-echo -e "${GREEN}"
-echo -e " [INFO] ${CYAN}Downloading Update File"
-sleep 2
-rm -rf menu
-rm -rf menu-ssh
-rm -rf menu-vmess
-rm -rf menu-vless
-rm -rf menu-trojan
-
+# LINE COLOUR
+line=$(cat /etc/line)
+# TEXT COLOUR BELOW
+below=$(cat /etc/below)
+# BACKGROUND TEXT COLOUR
+back_text=$(cat /etc/back)
+# NUMBER COLOUR
+number=$(cat /etc/number)
+# TEXT ON BOX COLOUR
+box=$(cat /etc/box)
+# CEK UPDATE
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info1="${Green_font_prefix}($version)${Font_color_suffix}"
+Info2="${Green_font_prefix}(LATEST VERSION)${Font_color_suffix}"
+Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
+version=$(cat /home/ver)
+new_version=$( curl https://raw.githubusercontent.com/${GitUser}/tunel/main/newversion | grep $version )
+#Status Version
+if [ $version = $new_version ]; then
+sts="${Info2}"
+else
+sts="${Error}"
+fi
 clear
-echo -e " [INFO] Downloading Update File"
+echo ""
+echo -e "   \e[$line--------------------------------------------------------\e[m"
+echo -e "   \e[$back_text                 \e[30m[\e[$box CHECK NEW UPDATE\e[30m ]                   \e[m"
+echo -e "   \e[$line--------------------------------------------------------\e[m"
+echo -e "   \e[$below VVERSION NOW >> $Info1"
+echo -e "   \e[$below SSTATUS UPDATE >> $sts"
+echo -e ""
+echo -e "       \e[1;31mWould you like to proceed?\e[0m"
+echo ""
+echo -e "            \e[0;32m[ Select Option ]\033[0m"
+echo -e "     \e[$number [1]\e[m \e[$below CCheck Script Update Now\e[m"
+echo -e "     \e[$number [x]\e[m \e[$below BBack To Menu\e[m"
+echo -e ""
+echo -e "   \e[$line--------------------------------------------------------\e[m"
+echo -e "\e[$line"
+read -p "PPlease Choose 1 or x : " option2
+case $option2 in
+1)
+version=$(cat /home/ver)
+new_version=$( curl https://raw.githubusercontent.com/${GitUser}/tunel/main/newversion | grep $version )
+if [ $version = $new_version ]; then
+clear
+echo ""
+echo -e "\e[1;31mChecking New Version, Please Wait...!\e[m"
+sleep 3
+clear
+echo -e "\e[1;31mUpdate Not Available\e[m"
+echo ""
+clear
+sleep 1
+echo -e "\e[1;36mYou Have The Latest Version\e[m"
+echo -e "\e[1;31mThankyou.\e[0m"
 sleep 2
+update
+fi
+clear
+echo -e "\e[1;31mUpdate Available Now..\e[m"
+echo -e ""
+sleep 2
+echo -e "\e[1;36mStart Update For New Version, Please Wait..\e[m"
+sleep 2
+clear
+echo -e "\e[0;32mGetting New Version Script..\e[0m"
+sleep 1
+echo ""
+# UPDATE RUN-UPDATE
+cd /usr/bin
+wget -O run-update "https://raw.githubusercontent.com/${GitUser}/tunel/main/options/update.sh"
+chmod +x run-update
+# RUN UPDATE
+echo ""
+clear
+echo -e "\e[0;32mPlease Wait...!\e[0m"
+sleep 6
+clear
+echo ""
+echo -e "\e[0;32mNew Version Downloading started!\e[0m"
+sleep 2
+cd /usr/bin
 
 wget -q -O /usr/bin/autoreboot "https://raw.githubusercontent.com/Jatimpark/tunel/main/options/autoreboot.sh"
 wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/Jatimpark/tunel/main/options/restart.sh"
@@ -46,7 +127,6 @@ chmod +x /usr/bin/clearlog
 chmod +x /usr/bin/running
 chmod +x /usr/bin/speedtest
 chmod +x /usr/bin/cek-bandwidth
-#chmod +x /usr/bin/limitspeed
 chmod +x /usr/bin/menu-vless
 chmod +x /usr/bin/menu-vmess
 chmod +x /usr/bin/menu-ss
@@ -63,8 +143,34 @@ chmod +x /usr/bin/menu-set
 chmod +x /usr/bin/info
 chmod +x /usr/bin/infoserv
 clear
-clear
-echo -e "${NC}"
-echo -e "${GREEN} [INFO] ${NC}Update Sukses Sayank..!!"
+echo -e ""
+echo -e "\e[0;32mDownloaded successfully!\e[0m"
+echo ""
+ver=$( curl https://raw.githubusercontent.com/${GitUser}/tunel/main/version )
+sleep 1
+echo -e "\e[0;32mPatching New Update, Please Wait...\e[0m"
+echo ""
 sleep 2
+echo -e "\e[0;32mPatching... OK!\e[0m"
+sleep 1
+echo ""
+echo -e "\e[0;32mSucces Update Script For New Version\e[0m"
+cd
+echo "$ver" > /home/ver
+rm -f update.sh
+clear
+echo ""
+echo -e "\033[0;34m----------------------------------------\033[0m"
+echo -e "\E[42;1;37m        SCRIPT UDAH UPDATED              \E[0m"
+echo -e "\033[0;34m----------------------------------------\033[0m"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
 menu
+;;
+x)
+clear
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+;;
+esac
