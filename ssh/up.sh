1716130891525fae058233f00c820b28697c041a
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2023-10-30 17:42:18 +0700

    Delete ssh/up.sh

--- ssh/up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-clear
-cd /usr/bin
-wget -O up-ins-ray "raw.githubusercontent.com/Jatimpark/tunel/main/tools/up-ins-xray.sh" && chmod +x up-ins-xray
-rm -fr up-ins-xray.sh
-echo -e "\e[0;32mSelesai... OK!\e[0m"
-sleep 2
-exit
