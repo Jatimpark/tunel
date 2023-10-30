e20cb27ab681a5dfe86176a11f47f74907360e32
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2023-10-30 16:26:55 +0700

    Add files via upload

--- ssh/up.sh
+++ ssh/up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 cd /usr/bin
-wget -O up-ins-xray "raw.githubusercontent.com/Jatimpark/tunel/main/tools/up-ins-xray.sh" && chmod +x up-ins-xray.sh
+wget -O up-ins-ray "raw.githubusercontent.com/Jatimpark/tunel/main/tools/up-ins-xray.sh" && chmod +x up-ins-xray
 rm -fr up-ins-xray.sh
 echo -e "\e[0;32mSelesai... OK!\e[0m"
 sleep 2
