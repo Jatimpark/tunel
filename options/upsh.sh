6b7c57726c0b3aaa2d39aa655c8927259d0bfe18
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2023-10-31 10:14:31 +0700

    Add files via upload

--- options/upsh.sh
+++ options/upsh.sh
@@ -64,7 +64,7 @@ clear
 cd /usr/bin
 
 wget -O up-ssh-vpn "raw.githubusercontent.com/Jatimpark/tunel/main/ssh/up-ssh-vpn.sh" && chmod +x up-ssh-vpn.sh
-up-ssh-vpn.sh
+up-ssh-vpn
 rm -fr up-ssh-vpn.sh
 echo -e "\033[0;32m Udah Sayank.. !! "
 sleep 2
