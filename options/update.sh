669c85a7e3955caae3730f95dd7764935c94a1bf
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-08-04 10:20:43 +0700

    Add files via upload

--- options/update.sh
+++ options/update.sh
@@ -1,123 +1,30 @@
 #!/bin/bash
-#wget https://github.com/${GitUser}/
-GitUser="Jatimpark"
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
-echo ""
-version=$(cat /home/ver)
-ver=$( curl https://raw.githubusercontent.com/${GitUser}/tunel/main/version )
-clear
-# LINE COLOUR
-line=$(cat /etc/line)
-# TEXT COLOUR BELOW
-below=$(cat /etc/below)
-# BACKGROUND TEXT COLOUR
-back_text=$(cat /etc/back)
-# NUMBER COLOUR
-number=$(cat /etc/number)
-# TEXT ON BOX COLOUR
-box=$(cat /etc/box)
-# CEK UPDATE
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info1="${Green_font_prefix}($version)${Font_color_suffix}"
-Info2="${Green_font_prefix}(LATEST VERSION)${Font_color_suffix}"
-Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
-version=$(cat /home/ver)
-new_version=$( curl https://raw.githubusercontent.com/${GitUser}/tunel/main/newversion | grep $version )
-#Status Version
-if [ $version = $new_version ]; then
-sts="${Info2}"
-else
-sts="${Error}"
-fi
-clear
-echo ""
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "   \e[$back_text                 \e[30m[\e[$box CHECK NEW UPDATE\e[30m ]                   \e[m"
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "   \e[$below VVERSION NOW >> $Info1"
-echo -e "   \e[$below SSTATUS UPDATE >> $sts"
-echo -e ""
-echo -e "       \e[1;31mWould you like to proceed?\e[0m"
-echo ""
-echo -e "            \e[0;32m[ Select Option ]\033[0m"
-echo -e "     \e[$number [1]\e[m \e[$below CCheck Script Update Now\e[m"
-echo -e "     \e[$number [x]\e[m \e[$below BBack To Menu\e[m"
-echo -e ""
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "\e[$line"
-read -p "PPlease Choose 1 or x : " option2
-case $option2 in
-1)
-version=$(cat /home/ver)
-new_version=$( curl https://raw.githubusercontent.com/${GitUser}/tunel/main/newversion | grep $version )
-if [ $version = $new_version ]; then
-clear
-echo ""
-echo -e "\e[1;31mChecking New Version, Please Wait...!\e[m"
-sleep 3
-clear
-echo -e "\e[1;31mUpdate Not Available\e[m"
-echo ""
-clear
-sleep 1
-echo -e "\e[1;36mYou Have The Latest Version\e[m"
-echo -e "\e[1;31mThankyou.\e[0m"
-sleep 2
-update
-fi
-clear
-echo -e "\e[1;31mUpdate Available Now..\e[m"
 echo -e ""
 sleep 2
-echo -e "\e[1;36mStart Update For New Version, Please Wait..\e[m"
-sleep 2
-clear
-echo -e "\e[0;32mGetting New Version Script..\e[0m"
-sleep 1
-echo ""
-# UPDATE RUN-UPDATE
-cd /usr/bin
-wget -O run-update "https://raw.githubusercontent.com/${GitUser}/tunel/main/options/update.sh"
-chmod +x run-update
-# RUN UPDATE
-echo ""
-clear
-echo -e "\e[0;32mPlease Wait...!\e[0m"
-sleep 6
-clear
-echo ""
-echo -e "\e[0;32mNew Version Downloading started!\e[0m"
-sleep 2
+echo -e "\033[0;32m Start Loading.......\033[0m"
+sleep 3. 
 cd /usr/bin
 
-wget -q -O /usr/bin/autoreboot "https://raw.githubusercontent.com/Jatimpark/tunel/main/options/autoreboot.sh"
-wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/Jatimpark/tunel/main/options/restart.sh"
-wget -q -O /usr/bin/clearlog "https://raw.githubusercontent.com/Jatimpark/tunel/main/options/clearlog.sh"
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/Jatimpark/tunel/main/options/running.sh"
-wget -q -O /usr/bin/speedtest "https://raw.githubusercontent.com/Jatimpark/tunel/main/tools/speedtest_cli.py"
-wget -q -O /usr/bin/cek-bandwidth "https://raw.githubusercontent.com/Jatimpark/tunel/main/options/cek-bandwidth.sh"
-wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/Jatimpark/tunel/main/menu/menu-vless.sh"
-wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/Jatimpark/tunel/main/menu/menu-vmess.sh"
-wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/Jatimpark/tunel/main/menu/menu-ss.sh"
-wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/Jatimpark/tunel/main/menu/menu-trojan.sh"
-wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Jatimpark/tunel/main/menu/menu-ssh.sh"
-wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/Jatimpark/tunel/main/menu/menu-backup.sh"
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Jatimpark/tunel/main/menu/menu.sh"
-wget -q -O /usr/bin/babi "https://raw.githubusercontent.com/Jatimpark/tunel/main/ssh/babi.sh"
-wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/Jatimpark/tunel/main/ssh/xp.sh"
-wget -q -O /usr/bin/update "https://raw.githubusercontent.com/Jatimpark/tunel/main/options/update.sh"
-wget -q -O /usr/bin/addhost "https://raw.githubusercontent.com/Jatimpark/tunel/main/menu/addhost.sh"
-wget -q -O /usr/bin/certxray "https://raw.githubusercontent.com/Jatimpark/tunel/main/menu/crt.sh"
-wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/Jatimpark/tunel/main/ssh/menu-set.sh"
-wget -q -O /usr/bin/info "https://raw.githubusercontent.com/Jatimpark/tunel/main/options/info.sh"
-wget -q -O /usr/bin/infoserv "https://raw.githubusercontent.com/Jatimpark/tunel/main/options/infoserv.sh"
+wget -q -O /usr/bin/autoreboot "https://raw.githubusercontent.com/afiaza/tunel/main/options/autoreboot.sh"
+wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/afiaza/tunel/main/options/restart.sh"
+wget -q -O /usr/bin/clearlog "https://raw.githubusercontent.com/afiaza/tunel/main/options/clearlog.sh"
+wget -q -O /usr/bin/running "https://raw.githubusercontent.com/afiaza/tunel/main/options/running.sh"
+wget -q -O /usr/bin/speedtest "https://raw.githubusercontent.com/afiaza/tunel/main/tools/speedtest_cli.py"
+wget -q -O /usr/bin/cek-bandwidth "https://raw.githubusercontent.com/afiaza/tunel/main/options/cek-bandwidth.sh"
+wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/afiaza/tunel/main/menu/menu-vless.sh"
+wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/afiaza/tunel/main/menu/menu-vmess.sh"
+wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/afiaza/tunel/main/menu/menu-ss.sh"
+wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/afiaza/tunel/main/menu/menu-trojan.sh"
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/afiaza/tunel/main/menu/menu-ssh.sh"
+wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/afiaza/tunel/main/menu/menu-backup.sh"
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/afiaza/tunel/main/menu/menu.sh"
+wget -q -O /usr/bin/babi "https://raw.githubusercontent.com/afiaza/tunel/main/ssh/babi.sh"
+wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/afiaza/tunel/main/ssh/xp.sh"
+wget -q -O /usr/bin/addhost "https://raw.githubusercontent.com/afiaza/tunel/main/menu/addhost.sh"
+wget -q -O /usr/bin/certxray "https://raw.githubusercontent.com/afiaza/tunel/main/menu/crt.sh"
+wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/afiaza/tunel/main/ssh/menu-set.sh"
+wget -q -O /usr/bin/info "https://raw.githubusercontent.com/afiaza/tunel/main/options/info.sh"
+wget -q -O /usr/bin/infoserv "https://raw.githubusercontent.com/afiaza/tunel/main/options/infoserv.sh"
 
 chmod +x /usr/bin/autoreboot
 chmod +x /usr/bin/restart
@@ -134,7 +41,6 @@ chmod +x /usr/bin/menu-backup
 chmod +x /usr/bin/menu
 chmod +x /usr/bin/babi.sh
 chmod +x /usr/bin/xp
-chmod +x /usr/bin/update
 chmod +x /usr/bin/addhost
 chmod +x /usr/bin/certxray
 chmod +x /usr/bin/menu-set
@@ -144,18 +50,8 @@ clear
 echo -e ""
 echo -e "\e[0;32mDownloaded successfully!\e[0m"
 echo ""
-ver=$( curl https://raw.githubusercontent.com/${GitUser}/tunel/main/version )
-sleep 1
-echo -e "\e[0;32mPatching New Update, Please Wait...\e[0m"
-echo ""
-sleep 2
-echo -e "\e[0;32mPatching... OK!\e[0m"
-sleep 1
-echo ""
-echo -e "\e[0;32mSucces Update Script For New Version\e[0m"
 cd
-echo "$ver" > /home/ver
-rm -f update.sh
+rm -fr update.sh
 clear
 echo ""
 echo -e "\033[0;34m----------------------------------------\033[0m"
