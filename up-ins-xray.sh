f04aeb4a9cac2360a6c77472d00311d58ecbdd42
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-08-04 11:40:12 +0700

    Add files via upload

--- up-ins-xray.sh
+++ up-ins-xray.sh
@@ -1,16 +1,19 @@
-    server {
-             listen 8000;
-             listen [::]:8000;
-             root /var/www/html;
-        }
+# nginx xray.conf
+rm -fr /etc/nginx/conf.d/xray.conf
+cat >/etc/nginx/conf.d/xray.conf <<EOF
     server {
              listen 80;
              listen [::]:80;
-             listen 443 ssl http2;
-             ssl_certificate /usr/local/etc/xray/fullchain.crt;
-             ssl_certificate_key /usr/local/etc/xray/private.key;
+             listen 443 ssl http2 reuseport;
+             listen [::]:443 http2 reuseport;	
+             server_name 127.0.0.1 localhost;
+             ssl_certificate /etc/xray/xray.crt;
+             ssl_certificate_key /etc/xray/xray.key;
              ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
              ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
+             root /home/vps/public_html;
+        }
+EOF
 location / {
 if ($http_upgrade != "Upgrade") {
 rewrite /(.*) /vmess break;
