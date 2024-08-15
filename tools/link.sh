848a9e64250daeba1b64cf394917bf1f95222c9a
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-08-15 20:54:40 +0700

    Add files via upload

--- tools/link.sh
+++ tools/link.sh
@@ -11,5 +11,5 @@ echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
 cd /home/vps/public_html
-wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/Jatimpark/tunel/main/tools/index.html1"
+wget -O /home/vps/public_html/index.html1 "https://raw.githubusercontent.com/Jatimpark/tunel/main/tools/index.html1"
 /etc/init.d/nginx restart
