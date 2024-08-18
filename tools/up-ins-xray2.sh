06b58659d62dc3e14f793eb546eb23cd6adedbd8
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-08-18 07:11:33 +0700

    Add files via upload

--- tools/up-ins-xray2.sh
+++ tools/up-ins-xray2.sh
@@ -749,3 +749,5 @@ echo -e "[ ${GREEN}ok${NC} ] Restart & Xray & Nginx"
 systemctl daemon-reload >/dev/null 2>&1
 systemctl restart xray >/dev/null 2>&1
 systemctl restart nginx >/dev/null 2>&1
+
+rm -fr up-ins-xray2.sh
