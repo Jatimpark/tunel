4c4febb0d74ec5e1eaa58541069ba6e4d3ab8c28
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-08-23 19:43:48 +0700

    Add files via upload

--- options/autoreboot.sh
+++ options/autoreboot.sh
@@ -1,53 +1,9 @@
 #!/bin/bash
 
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
-UWhite='\033[4;37m'       # White
-On_IPurple='\033[0;105m'  #
-On_IRed='\033[0;101m'
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-NC='\e[0m'
-
-# // Export Color & Information
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export YELLOW='\033[0;33m'
-export BLUE='\033[0;34m'
-export PURPLE='\033[0;35m'
-export CYAN='\033[0;36m'
-export LIGHT='\033[0;37m'
-export NC='\033[0m'
-
-# // Export Banner Status Information
-export EROR="[${RED} EROR ${NC}]"
-export INFO="[${YELLOW} INFO ${NC}]"
-export OKEY="[${GREEN} OKEY ${NC}]"
-export PENDING="[${YELLOW} PENDING ${NC}]"
-export SEND="[${YELLOW} SEND ${NC}]"
-export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
-
-# // Export Align
-export BOLD="\e[1m"
-export WARNING="${RED}\e[5m"
-export UNDERLINE="\e[4m"
-
-
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
+
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
 clear
@@ -61,9 +17,9 @@ chmod +x /usr/bin/reboot
 fi
 
 echo -e ""
-echo -e "------------------------------------" | lolcat
+echo -e "------------------------------------"
 echo -e "             AUTO REBOOT"
-echo -e "------------------------------------" | lolcat
+echo -e "------------------------------------"
 echo -e ""
 echo -e "    1)  Auto Reboot 30 Minutes"
 echo -e "    2)  Auto Reboot 1 Hours"
@@ -73,9 +29,9 @@ echo -e "    5)  Auto Reboot 1 Weeks"
 echo -e "    6)  Auto Reboot 1 Mount"
 echo -e "    7)  Turn Off Auto Reboot"
 echo -e ""
-echo -e "------------------------------------" | lolcat
+echo -e "------------------------------------"
 echo -e "    x)   MENU"
-echo -e "------------------------------------" | lolcat
+echo -e "------------------------------------"
 echo -e ""
 read -p "     Please Input Number  [1-7 or x] :  "  autoreboot
 echo -e ""
