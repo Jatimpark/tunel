d06ba23d0e1a64c93d5f2e3f3c1a463db4c717cd
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-09-01 00:06:47 +0700

    Add files via upload

--- options/restart.sh
+++ options/restart.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
 
 NC='\e[0m'
-COLOR1='\033[0;34m' 
-COLBG1='\033[0;33m' 
-echo -e "$COLOR1┌──────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}          SERVER PORT INFORMATION       ${NC} $COLOR1│$NC"       
-echo -e "$COLOR1└──────────────────────────────────────────┘${NC}"
-echo -e "\033[1;93m  >Port SSH Websocket       :80\e[0m"
-echo -e "\033[1;93m  >Port SSH Websocket SSL   :443\e[0m"
-echo -e "\033[1;93m  >Port SSH SSL             :222,777\e[0m"
-echo -e "\033[1;93m  >Port OpenSSH             :22\e[0m"
-echo -e "\033[1;93m  >Port SSH Dropbear        :109,143\e[0m"
-echo -e "\033[1;93m  >Port Xray None TLS       :80\e[0m"
-echo -e "\033[1;93m  >Port Xray TLS            :443\e[0m"
-echo -e "\033[1;93m  >Port Vmess None TLS      :80\e[0m"
-echo -e "\033[1;93m  >Port Vmess TLS           :443\e[0m"
-echo -e "\033[1;93m  >Port Vmess GRPC          :443\e[0m"
-echo -e "\033[1;93m  >Port Vless None TLS      :80\e[0m"
-echo -e "\033[1;93m  >Port Vless TLS           :443\e[0m"
-echo -e "\033[1;93m  >Port Vless GRPC          :443\e[0m"
-echo -e "\033[1;93m  >Port Trojan WS           :443\e[0m"
-echo -e "\033[1;93m  >Port Trojan GRPC         :443\e[0m"
-echo -e "\033[1;93m  >Port ShadowSocks WS      :443\e[0m"
-echo -e "\033[1;93m  >Port ShadowSocks GRPC    :443\e[0m"
-echo -e "\033[1;94m >>>>Service Support Change Port<<<< \e[0m"
-echo -e ""
-echo -e "\033[1;97mOrder AutoScript Lifetime wa.me/6287721815317\e[0m"
-echo -e ""
-
+green='\033[0;32m' 
 clear
-echo -e ""
+echo -e "${green}"
 echo -e "======================================"
 echo -e ""
-echo -e ""
+echo -e "${NC}
 echo -e "    [1] Restart All Services"
 echo -e "    [2] Restart OpenSSH"
 echo -e "    [3] Restart Dropbear"
@@ -42,8 +16,8 @@ echo -e "    [6] Restart Badvpn"
 echo -e "    [x] Menu"
 echo -e ""
 read -p "    Select From Options [1-6 or x] :  " Restart
-echo -e ""
-echo -e "======================================"
+echo -e "{green}"
+echo -e "======================================${NC}"
 sleep 1
 clear
 case $Restart in
@@ -58,14 +32,14 @@ case $Restart in
                 /etc/init.d/fail2ban restart
                 /etc/init.d/cron restart
                 /etc/init.d/nginx restart
-                # Restart service openvpn
+                #Restart service openvpn
                 #systemctl enable openvpn
                 #systemctl start openvpn
                 #/etc/init.d/openvpn restart
-                #screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
-                #screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
-                #screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
-                #systemctl restart rc-local.service
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
+                systemctl restart rc-local.service
                 echo -e ""
                 echo -e "======================================"
                 echo -e ""
