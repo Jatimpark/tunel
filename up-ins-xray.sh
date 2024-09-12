7b64e4ac3fcac5906b8392e6da92d059e2303fae
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-09-12 11:58:46 +0700

    Add files via upload

--- up-ins-xray.sh
+++ up-ins-xray.sh
@@ -159,7 +159,7 @@ latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases |
 # / / Installation Xray Core
 xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
 # / / Ambil Xray Core Version Terbaru
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.16
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.21
 
 # / / Make Main Directory
 
