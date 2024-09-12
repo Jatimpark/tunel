6ca8118688d41e1b484e7faa0997cd44c6df4792
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-09-12 16:42:31 +0700

    Update up-ins-xray.sh

--- tools/up-ins-xray.sh
+++ tools/up-ins-xray.sh
@@ -159,7 +159,7 @@ latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases |
 # / / Installation Xray Core
 xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
 # / / Ambil Xray Core Version Terbaru
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version $latest_version >/dev/null 2>&1
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.23
 
 # / / Make Main Directory
 
