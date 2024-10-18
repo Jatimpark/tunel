8bcaf0373de3536a9bde1f7258031058cb25457b
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-10-18 22:09:53 +0700

    Add files via upload

--- up-ins-xray.sh
+++ up-ins-xray.sh
@@ -381,7 +381,7 @@ cat <<EOF> /etc/xray/config.json
        "streamSettings":{
          "network": "ws",
             "wsSettings": {
-                "path": "/vmess"
+                "path": "/servlets/mms/vmess"
           }
         }
      },
